01cecd2ff1aa3a2cb38ca31fbab4e8553341ecf6
reb00ter <uhfybwsUtnn0>
2017-12-21 00:52:41 +0300

    ACSIIFileStorage

--- deztroy/settings.py
+++ deztroy/settings.py
@@ -122,6 +122,9 @@ STATIC_FILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+DEFAULT_FILE_STORAGE = 'deztroy.utils.ASCIIFileSystemStorage'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
---
+++ deztroy/utils.py
@@ -0,0 +1,16 @@
+from Scripts import unicodedata
+
+import pytils as pytils
+from django.core.files.storage import FileSystemStorage
+
+
+class ASCIIFileSystemStorage(FileSystemStorage):
+    """
+    Convert unicode characters in name to ASCII characters.
+    """
+
+    def get_valid_name(self, name):
+        name_parts = name.split('.')
+        name = unicodedata.normalize('NFKD', pytils.translit.slugify(name_parts[0])).encode('ascii', 'ignore').decode()
+        name = name + '.' + name_parts[-1]
+        return super(ASCIIFileSystemStorage, self).get_valid_name(name)
